4c24f614187c08829e5b4df2b4f9b3e56a62021d
Nishant Kumar <162552761+Nishant1121@users.noreply.github.com>
2026-06-12 08:52:04 +0000

    Datatypes Done

---
+++ basics-js/datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using node.js, not browser
+
+console.log(3
+    +3) // code readability should be high i.e indentation
+console.log(" Nishant");
+
+let name = 'Nishant'
+let age = 22
+let isLoggedIn = false
+let state = null
+let city
+
+// Primitive DataTypes
+//number => 2^53
+//bigint => use when have to store large number/integer value
+//string =>
+//boolean => true/false
+//null => standalone value
+//undefined => value is not assigned
+/*symbol => unique(when we make multiple components
+ and we have to identfiy which one is unique)*/
+
+// object
+console.log(typeof(null)); // Output and type is object
+console.log(typeof(undefined)); //Output and type is undefined  
+
+
