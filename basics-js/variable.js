db852ed36a39714b6a7601b955891c1fe9072737
Nishant Kumar <162552761+Nishant1121@users.noreply.github.com>
2026-06-12 08:06:19 +0000

    This is about Variable

---
+++ basics-js/variable.js
@@ -0,0 +1,22 @@
+const Number = 12345
+let email= 'nishant@12345';
+var mail = "nishant@124" 
+city = "varnasi"
+let state;
+//  Number=54321 we can't change const variable
+email = "neeraj@pepsu"
+mail = "gk@gmail";
+city = "lucknow"
+
+console.log(Number);
+console.log(email);
+console.log(mail);
+console.log(city)
+console.log (typeof(city));
+
+/* prefer not to use var
+   becuase of issue in block of scope and functional scope
+   */
+
+console.table([Number, email, mail, city,state])
+
