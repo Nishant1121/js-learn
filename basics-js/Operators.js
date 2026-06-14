1c5a51f9c31f9bdf5d2316bb34d112c8521c00bd
Nishant Kumar <162552761+Nishant1121@users.noreply.github.com>
2026-06-14 18:25:58 +0000

    operators

---
+++ basics-js/Operators.js
@@ -0,0 +1,16 @@
+let Value = 3
+let NegValue = -Value 
+console.log(NegValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/2);
+console.log(2%2);
+
+let str1 = "Nishant"
+let str2 = "Kumar"
+console.log(str1 + str2);
+console.log(str3);
+
--- basics-js/conversion.js
+++ basics-js/conversion.js
@@ -20,7 +20,4 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
-//************************Operations*********************//
-
-
 
