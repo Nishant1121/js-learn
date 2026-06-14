17f146a54a63f406cd6ad3d350d68ad8c1113cfa
Nishant Kumar <162552761+Nishant1121@users.noreply.github.com>
2026-06-14 18:10:50 +0000

    conversion Operations

--- basics-js/conversionOperation.js
+++ basics-js/conversionOperation.js
@@ -1,7 +1,26 @@
 let score = null
+// let score = true; // then it will give boolean
 console.log(typeof score);
 console.log(typeof(score));
 
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
+// "33" => 33
+// "33abc" => NaN
+// true => 1  false => 0 
+
+let isLoggedIn = 1 // if " " => false; if 0 or anyother value ("Nishant") then true
+let booleanIsLoggedIn = Boolean(isLoggedIn)   
+console.log(booleanIsLoggedIn);
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+//************************Operations*********************//
+
+
+
