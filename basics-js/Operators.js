143b80dfacb9f7aecb9442295147cb5e34d1b2d0
Nishant Kumar <162552761+Nishant1121@users.noreply.github.com>
2026-06-15 18:15:04 +0000

    operators concepts

--- basics-js/Operators.js
+++ basics-js/Operators.js
@@ -12,5 +12,12 @@ console.log(2%2);
 let str1 = "Nishant"
 let str2 = "Kumar"
 console.log(str1 + str2);
-console.log(str3);
+// console.log(str3);
+
+console.log("1"+ 2);//op -> 12
+console.log(1+ "2"); //op -> 12
+console.log("1"+2+2); //op -> 122
+console.log(1+2+"2"); //op -> 32
+
+
 
