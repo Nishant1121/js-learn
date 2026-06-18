aa9a9df2fbffa3ce21ffa6c4ab9c2017bdab212b
Nishant Kumar <162552761+Nishant1121@users.noreply.github.com>
2026-06-18 16:53:54 +0000

    Conditions in JS

---
+++ basics-js/condition.js
@@ -0,0 +1,48 @@
+// let age =18
+
+// if (age >= 18) {
+//     console.log("You are eligible to vote.");
+// } else {
+//     console.log("You are not eligible to vote.");
+// }
+
+//Nested If
+
+// let age = 16
+// let country = "India"
+// let text = " You Can't Drive"
+// if(country=="India"){
+
+// if (age>=16)
+// {
+// console.log(" You Can Drive");
+
+// }}
+
+/*if Else
+Syntax
+ if (condition1) {
+    block of code to be executed if condition1 is true
+ } else if (condition2) {
+    block of code to be executed if the condition1 is false and condition2 is true
+ } else {
+    block of code to be executed if the condition1 is false and condition2 is false
+ }*/
+let age = 18
+if (age>=18)
+{
+    console.log("User is Eligibe for Vote")
+}
+else
+{
+    console.log("User is Not Eligible for Vote")
+}
+
+/*Ternary Operator
+Syntax
+ (condition) ? expression1 : expression2 */
+let ag = 18;
+(age < 18) ? "Minor" : "Adult";
+
+
+
