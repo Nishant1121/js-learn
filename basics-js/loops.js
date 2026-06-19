46c8cb837b71c912ca0577cd890fe8985d89a5ca
Nishant Kumar <162552761+Nishant1121@users.noreply.github.com>
2026-06-19 17:01:46 +0000

    Learning loops

---
+++ basics-js/loops.js
@@ -0,0 +1,34 @@
+/* Syntax -> For
+for (expr1; expr2; expr3) {
+  code block to be executed
+}*/
+
+for(let i = 1;i<=10;i++){
+console.log("Natural Number is ",i);
+}
+
+/* Syntax -> While
+while (condition) {
+code block to be executed
+}
+*/
+let j=0
+while( j<=10)
+{
+j++
+console.log(j);
+}
+
+/* Syntax -> Do-While
+do {
+  // code block to be executed
+}
+while (condition); */
+let text
+let k =10
+do {
+  text += "The number is " + k;
+  k++;
+}
+while (k < 10);
+console.log(text)
