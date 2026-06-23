5abce390149cb20816f142632bf4c586f00ee1ae
Nishant Kumar <162552761+Nishant1121@users.noreply.github.com>
2026-06-23 16:53:56 +0000

    About String

---
+++ basics-js/strings.js
@@ -0,0 +1,19 @@
+const Name = "Nishant"
+const Roll = " 122"
+
+// console.log(Name+Roll+ " MCA");
+
+console.log(`Hello My Name is ${Name} and my Roll Number is ${Roll}`);
+
+const MyName = new String('NishantK')
+ console.log(MyName[0]);
+ console.log(MyName[3]);
+ console.log(MyName.__proto__);
+ console.log(MyName.length);
+ console.log(MyName.toUpperCase());
+ console.log(MyName.charAt(3));
+ 
+ 
+ 
+ 
+ 
