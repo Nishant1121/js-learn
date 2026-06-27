5c41657ba8ad0ec74d500b091b0640400696a930
Nishant Kumar <162552761+Nishant1121@users.noreply.github.com>
2026-06-27 18:01:50 +0000

    Learned About Set

---
+++ basics-js/set.js
@@ -0,0 +1,41 @@
+/*A JavaScript Set is a collection of unique values.
+Each value can only occur once in a Set.
+The values can be of any type, primitive values or objects.*/
+
+/*      Set Methods
+new Set()
+add()
+clear()
+delete()
+entries()
+forEach()
+has()
+keys()
+values()
+size*/
+
+//create a set
+// const letters = new Set(["a","b","c"]);
+// console.log(letters);
+
+// Create a Set
+const letters = new Set();
+
+// Add Values to the Set
+letters.add("a");
+letters.add("b");
+letters.add("c");
+letters.delete("a")
+
+console.log(letters);
+
+
+/*
+         Logic Set Method
+union()
+difference()
+intersection()
+isDisjointFrom()
+isSubsetOf()
+isSupersetOf()
+symmetricDifference()*/
