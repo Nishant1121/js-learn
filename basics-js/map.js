6d63dce6079a44bc4ab050783cac3da8454ebcd9
Nishant Kumar <162552761+Nishant1121@users.noreply.github.com>
2026-06-29 17:03:49 +0000

    about maps set and get in js

--- basics-js/map.js
+++ basics-js/map.js
@@ -15,3 +15,20 @@ const fruits = new Map([
 
 console.log(fruits);
 console.table(fruits);
+
+
+// You can add elements to a Map with the set() method:
+
+fruits.set("mangos", 100);
+
+// The set() method can also be used to change existing Map values:
+
+fruits.set("apples", 200);
+
+// The get() method gets the value of a key in a Map:
+
+fruits.get("apples");    // Returns 500
+
+console.log( typeof (fruits));
+
+console.table(fruits);
